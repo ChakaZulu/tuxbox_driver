012be376f5559eff92bc1ed5ccdf5cf9b4f79c04
gillem <gillem>
2001-01-04 20:12:36 +0000

    *** empty log message ***

--- saa7126/saa7126_calc.c
+++ saa7126/saa7126_calc.c
@@ -2,12 +2,13 @@
 
 void cyc2rgb( int cr, int y, int cb, int *r, int *g, int *b )
 {
-	*r = y + 1.3707*(cr-128);
+/*	*r = y + 1.3707*(cr-128);
 	*g = y - 0.3365*(cb-128) - 0.6982*(cr-128);
 	*b = y + 1.7324*(cb-128);
+*/
 }
 
-unsigned int fsc( float ffsc, float fiic )
+unsigned int fsc( int ffsc, int fiic )
 {
-	return((ffsc/fiic)*(2^32));
+	return((ffsc/fiic)*(0x100000000));
 }
