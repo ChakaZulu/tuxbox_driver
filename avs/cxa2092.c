f64c2e0eb9674bc9708ea4b070a7882417f6c943
gillem <gillem>
2001-01-15 17:02:32 +0000

    rewriten

--- avs/cxa2092.c
+++ avs/cxa2092.c
@@ -1,5 +1,5 @@
 /*
- *   cxa2092.c - scartswitch driver (dbox-II-project)
+ *   cxa2092.c - audio/video switch driver (dbox-II-project)
  *
  *   Homepage: http://dbox2.elxsi.de
  *
@@ -21,10 +21,13 @@
  *
  *
  *   $Log: cxa2092.c,v $
+ *   Revision 1.4  2001/01/15 17:02:32  gillem
+ *   rewriten
+ *
  *   Revision 1.3  2001/01/06 10:05:43  gillem
  *   cvs check
  *
- *   $Revision: 1.3 $
+ *   $Revision: 1.4 $
  *
  */
 
@@ -60,24 +63,27 @@ static struct i2c_client_address_data addr_data = {
 
 ///////////////////////////////////////////////////////////////////////////////
 
-static int scartswitch_ioctl (struct inode *inode, struct file *file,
+static int avs_ioctl (struct inode *inode, struct file *file,
                          unsigned int cmd, unsigned long arg);
-static int scartswitch_open (struct inode *inode, struct file *file);
+static int avs_open (struct inode *inode, struct file *file);
+
+static int avs_set(void);
 
 ///////////////////////////////////////////////////////////////////////////////
 
-static struct file_operations scartswitch_fops = {
+static struct file_operations avs_fops = {
 	owner:		THIS_MODULE,
 //	read:		lcd_read,
 //	write:		lcd_write,
-	ioctl:		scartswitch_ioctl,
-	open:		scartswitch_open,
+	ioctl:		avs_ioctl,
+	open:			avs_open,
 };
 
 ///////////////////////////////////////////////////////////////////////////////
 
-#define I2C_DRIVERID_SCARTSWITCH	1
-#define SCARTSWITCH_MAJOR 				40
+#define AVS_COUNT					1
+#define I2C_DRIVERID_AVS	1
+#define AVS_MAJOR 				40
 
 static int debug =  0; /* insmod parameter */
 static int type  =  -1;
@@ -102,90 +108,226 @@ void schedule_timeout(int j)
 }
 #endif
 
-struct scartswitch
+struct avs
 {
-	int type;           /* chip type */
-	int data1;					/* current settings */
-	int data2;
-	int data3;
-	int data4;
+	int type;           		/* chip type */
 };
 
 static struct i2c_driver driver;
 static struct i2c_client client_template;
 
+static int avs_status;	/* current status */
+static unsigned char avs_data[5];	/* current settings */
+
 /* ---------------------------------------------------------------------- */
 
-struct scartswitchtype
+struct avs_type
 {
 	char *name;
 	u8 Vendor;
 	u8 Type;
 };
 
-static struct scartswitchtype scartswitch[] = {
+static struct avs_type avs_types[] = {
 	{"CXA2092", 0, 0 }
 };
 
 /* ---------------------------------------------------------------------- */
 
-static int scartswitch_getstatus(struct i2c_client *c)
+void avs_set_volume( int vol )
+{
+	int c=0,f=0;
+
+	if (vol&(vol<=56))
+	{
+		c = vol/8;
+		f = vol-c;
+	}
+
+	SET_EVC(avs_data,c);
+	SET_EVF(avs_data,f);
+}
+
+void avs_set_mute( int type )
+{
+	switch(type)
+	{
+		case AVS_MUTE_IM:
+		case AVS_MUTE_ZC:
+		case AVS_UNMUTE_IM:
+		case AVS_UNMUTE_ZC:
+			avs_data[0] = (avs_data[0]&(~3)) | type;
+			break;
+		default:
+			break;
+	}
+}
+
+void avs_set_fblk( int type )
+{
+	switch(type)
+	{
+		case AVS_FBLKOUT_NULL:
+		case AVS_FBLKOUT_5V:
+		case AVS_FBLKOUT_IN1:
+		case AVS_FBLKOUT_IN2:
+			avs_data[1] = (avs_data[1]&(~AVS_FBLK)) | (type<<6);
+			break;
+		default:
+			break;
+	}
+}
+
+void avs_set_fnc( int type )
+{
+	switch(type)
+	{
+		case AVS_FNCOUT_INTTV:
+		case AVS_FNCOUT_EXT169:
+		case AVS_FNCOUT_EXT43:
+		case AVS_FNCOUT_EXT43_1:
+			avs_data[2] = (avs_data[2]&(~AVS_FNC)) | (type<<6);
+			break;
+		default:
+			break;
+	}
+}
+
+void avs_set_ycm( int type )
+{
+	switch(type)
+	{
+		case 0:
+		case 1:
+			avs_data[3] = (avs_data[3]&(~AVS_YCM)) | (type<<7);
+			break;
+		default:
+			break;
+	}
+
+}
+
+void avs_set_vsw( int sw, int type )
+{
+	if(sw<0 || sw>2)
+		return;
+
+	switch(type)
+	{
+		case AVS_TVOUT_DE1:
+		case AVS_TVOUT_DE2:
+		case AVS_TVOUT_VCR:
+		case AVS_TVOUT_AUX:
+		case AVS_TVOUT_DE3:
+		case AVS_TVOUT_DE4:
+		case AVS_TVOUT_DE5:
+		case AVS_TVOUT_VM1:
+			avs_data[sw+1] = (avs_data[sw+1]&(~AVS_VSW1)) | (type<<3);
+			break;
+		default:
+			break;
+	}
+}
+
+void avs_set_asw( int sw, int type )
+{
+	if(sw<0 || sw>2)
+		return;
+
+	switch(type)
+	{
+		case AVS_AUXOUT_DE1:
+		case AVS_AUXOUT_VM1:
+		case AVS_AUXOUT_VCR:
+		case AVS_AUXOUT_AUX:
+		case AVS_AUXOUT_DE2:
+		case AVS_AUXOUT_VM2:
+		case AVS_AUXOUT_VM3:
+		case AVS_AUXOUT_VM4:
+			avs_data[sw+1] = (avs_data[sw+1]&(~AVS_ASW1)) | type;
+			break;
+		default:
+			break;
+	}
+}
+
+void avs_set_logic( int type )
+{
+	return;
+}
+
+int avs_set()
+{
+	if ( 5 != i2c_master_send(&client_template, avs_data, 5))
+		return -EFAULT;
+
+	return 0;
+}
+/* ---------------------------------------------------------------------- */
+
+static int avs_getstatus(struct i2c_client *c)
 {
 	unsigned char byte;
 
 	if (1 != i2c_master_recv(c,&byte,1))
-		return 0;
+		return -1;
+
 	return byte;
 }
 
 /* ---------------------------------------------------------------------- */
 
-static int scartswitch_attach(struct i2c_adapter *adap, int addr,
+static int avs_attach(struct i2c_adapter *adap, int addr,
 			unsigned short flags, int kind)
 {
-	struct scartswitch *t;
+	struct avs *t;
 	struct i2c_client *client;
 
-	dprintk("[SCSW]: attach\n");
+	dprintk("[AVS]: attach\n");
 
 	if (this_adap > 0)
 		return -1;
+
 	this_adap++;
 	
-        client_template.adapter = adap;
-        client_template.addr = addr;
+  client_template.adapter = adap;
+  client_template.addr = addr;
 
-        dprintk("scartswitch: chip found @ 0x%x\n",addr);
+  dprintk("[AVS]: chip found @ 0x%x\n",addr);
 
-        if (NULL == (client = kmalloc(sizeof(struct i2c_client), GFP_KERNEL)))
-                return -ENOMEM;
-        memcpy(client,&client_template,sizeof(struct i2c_client));
-        client->data = t = kmalloc(sizeof(struct scartswitch),GFP_KERNEL);
-        if (NULL == t) {
-                kfree(client);
-                return -ENOMEM;
-        }
-        memset(t,0,sizeof(struct scartswitch));
+  if (NULL == (client = kmalloc(sizeof(struct i2c_client), GFP_KERNEL)))
+		return -ENOMEM;
 
-	if (type >= 0 && type < SCARTSWITCHES) {
+  memcpy(client,&client_template,sizeof(struct i2c_client));
+  client->data = t = kmalloc(sizeof(struct avs),GFP_KERNEL);
+
+  if (NULL == t) {
+		kfree(client);
+    return -ENOMEM;
+	}
+
+  memset(t,0,sizeof(struct avs));
+
+	if (type >= 0 && type < AVS_COUNT) {
 		t->type = type;
-		strncpy(client->name, scartswitch[t->type].name, sizeof(client->name));
+		strncpy(client->name, avs_types[t->type].name, sizeof(client->name));
 	} else {
 		t->type = -1;
 	}
-        i2c_attach_client(client);
+
+	i2c_attach_client(client);
 	//MOD_INC_USE_COUNT;
 
 	return 0;
 }
 
-static int scartswitch_probe(struct i2c_adapter *adap)
+static int avs_probe(struct i2c_adapter *adap)
 {
 	int ret;
 
 	ret = 0;
 
-	dprintk("[SCSW]: probe\n");
+	dprintk("[AVS]: probe\n");
 
 	if (0 != addr) {
 		normal_i2c_range[0] = addr;
@@ -194,21 +336,19 @@ static int scartswitch_probe(struct i2c_adapter *adap)
 
 	this_adap = 0;
 	
-	//if (adap->id == (I2C_ALGO_BIT | I2C_HW_B_BT848))
-
 	if (1)
-		ret = i2c_probe(adap, &addr_data, scartswitch_attach );
+		ret = i2c_probe(adap, &addr_data, avs_attach );
 
-	dprintk("[SCSW]: probe end %d\n",ret);
+	dprintk("[AVS]: probe end %d\n",ret);
 
 	return ret;
 }
 
-static int scartswitch_detach(struct i2c_client *client)
+static int avs_detach(struct i2c_client *client)
 {
-	struct scartswitch *t = (struct scartswitch*)client->data;
+	struct avs *t = (struct avs*)client->data;
 
-	dprintk("[SCSW]: detach\n");
+	dprintk("[AVS]: detach\n");
 
 	i2c_detach_client(client);
 	kfree(t);
@@ -216,70 +356,82 @@ static int scartswitch_detach(struct i2c_client *client)
 	return 0;
 }
 
-static int scartswitch_command(struct i2c_client *client, unsigned int cmd, void *arg )
+static int avs_command(struct i2c_client *client, unsigned int cmd, void *arg )
 {
-	dprintk("[SCSW]: command\n");
+	dprintk("[AVS]: command\n");
 	return 0;
 }
 ///////////////////////////////////////////////////////////////////////////////
 
-int scartswitch_ioctl (struct inode *inode, struct file *file, unsigned int cmd,
+int avs_ioctl (struct inode *inode, struct file *file, unsigned int cmd,
                   unsigned long arg)
 {
-	int i;
-	static unsigned char buf[5];
-  struct scsw_switch *scsw_sw;
-	char sw_buf[] = { 0x0d,0x29,0xc9,0xa9,0x00 };
-
-	dprintk("[SCSW]: IOCTL\n");
+	int val;
+	dprintk("[AVS]: IOCTL\n");
 	
 	switch (cmd)
 	{
-		case IOCTL_WRITE_CONTROL_REG:
-			if ( copy_from_user( buf, (unsigned char*)arg, 5 ) )
+		case AVSIOSVSW1:
+			if ( get_user(val,(int*)arg) )
 				return -EFAULT;
-
-			dprintk("[SCSW]: send\n");
-
-			if ( 5 != i2c_master_send(&client_template, buf ,5))
-			{
+			avs_set_vsw(0,val);
+			avs_set();
+			break;
+		case AVSIOSVSW2:
+			if ( get_user(val,(int*)arg) )
 				return -EFAULT;
-			}
-
-			dprintk("[SCSW]: send ready \n");
-
+			avs_set_vsw(1,val);
+			avs_set();
+			break;
+		case AVSIOSVSW3:
+			if ( get_user(val,(int*)arg) )
+				return -EFAULT;
+			avs_set_vsw(2,val);
+			avs_set();
+			break;
+		case AVSIOSASW1:
+			if ( get_user(val,(int*)arg) )
+				return -EFAULT;
+			avs_set_asw(0,val);
+			avs_set();
+			break;
+		case AVSIOSASW2:
+			if ( get_user(val,(int*)arg) )
+				return -EFAULT;
+			avs_set_asw(1,val);
+			avs_set();
+			break;
+		case AVSIOSASW3:
+			if ( get_user(val,(int*)arg) )
+				return -EFAULT;
+			avs_set_asw(2,val);
+			avs_set();
+			break;
+		case AVSIOSVOL:
+			if ( get_user(val,(int*)arg) )
+				return -EFAULT;
+			avs_set_volume(val);
+			avs_set();
+			break;
+		case AVSIOSMUTE:
+			if ( get_user(val,(int*)arg) )
+				return -EFAULT;
+			avs_set_mute(val);
+			avs_set();
 			break;
 
-		case IOCTL_READ_STATUS:
-
-			buf[0] = 0;
-			buf[1] = 0;
-
-			if ( 2 != i2c_master_recv(&client_template, buf ,2))
-			{
-				return -EFAULT;
-			}
-
-			i = buf[0]&0xFF;
-
-			if ( copy_to_user( (int*)arg, &i, sizeof(int) ) )
-				return -EFAULT;
-
+		case AVSIOGVSW1:
+		case AVSIOGVSW2:
+		case AVSIOGVSW3:
+		case AVSIOGASW1:
+		case AVSIOGASW2:
+		case AVSIOGASW3:
+		case AVSIOGVOL:
+		case AVSIOGMUTE:
 			break;
 
-		case IOCTL_SWITCH:
-			scsw_sw = (scsw_switch*)arg;
-			
-			if (scsw_sw->out==SCSW_OUT1)
-				sw_buf[1] = scsw_sw->inp;
-			else
-				sw_buf[2] = scsw_sw->inp;
-
-			if ( 5 != i2c_master_recv(&client_template, sw_buf ,5))
-			{
-				return -EFAULT;
-			}
-
+		default:
+			return -EINVAL;
 			break;
 	}
 
@@ -288,7 +440,7 @@ int scartswitch_ioctl (struct inode *inode, struct file *file, unsigned int cmd,
 
 ///////////////////////////////////////////////////////////////////////////////
 
-int scartswitch_open (struct inode *inode, struct file *file)
+int avs_open (struct inode *inode, struct file *file)
 {
 	return 0;
 }
@@ -308,21 +460,22 @@ void dec_use (struct i2c_client *client)
         MOD_DEC_USE_COUNT;
 #endif
 }
+
 static struct i2c_driver driver = {
-        "i2c scart switch driver",
-        I2C_DRIVERID_SCARTSWITCH,
+        "i2c audio/video switch driver",
+        I2C_DRIVERID_AVS,
         I2C_DF_NOTIFY,
-        scartswitch_probe,
-        scartswitch_detach,
-        scartswitch_command,
+        avs_probe,
+        avs_detach,
+        avs_command,
         inc_use,
         dec_use,
 };
 
 static struct i2c_client client_template =
 {
-        "i2c scart switch chip",          /* name       */
-        I2C_DRIVERID_SCARTSWITCH,           /* ID         */
+        "i2c audio/video switch chip",          /* name       */
+        I2C_DRIVERID_AVS,           /* ID         */
         0,
 				0, /* interpret as 7Bit-Adr */
         NULL,
@@ -334,41 +487,22 @@ EXPORT_NO_SYMBOLS;
 #ifdef MODULE
 int init_module(void)
 #else
-int i2c_scartswitch_init(void)
+int i2c_avs_init(void)
 #endif
 {
-	int i;
-
-	/* shameless stolen from br ;-) */
-
-/* ADDR
- * 0    ???
- * 1    SCART 1
- * 2    SCART 2
- * 3    ???
- * 4    ???
- */
-	char buf[] = {
-	0x0d,0x29,0xc9,0xa9,0x00,
-	0x0f,0x29,0xc9,0xa9,0x80,
-	0x0f,0x2f,0xcf,0xaf,0x80,
-	0x0f,0x29,0xc9,0xa9,0x80,
-	0x0f,0x29,0xc9,0xa9,0x00};
-
 	i2c_add_driver(&driver);
 
-	if (register_chrdev(SCARTSWITCH_MAJOR,"scartswitch",&scartswitch_fops)) {
-		printk("cxa2092.o: unable to get major %d\n", SCARTSWITCH_MAJOR);
+	if (register_chrdev(AVS_MAJOR,"avs",&avs_fops)) {
+		printk("cxa2092.o: unable to get major %d\n", AVS_MAJOR);
 		return -EIO;
 	}
 
-  for(i=0;i<5;i++)
-	{
-		if ( 5 != i2c_master_send(&client_template, buf+(i*5),5))
-		{
-			return -EFAULT;
-		}
-	}
+	memset(avs_data,0,5);
+
+	if ( 5 != i2c_master_send(&client_template, avs_data, 5))
+		return -EFAULT;
+
+//	avs_status = avs_getstatus(&client_template);
 
 	return 0;
 }
@@ -378,8 +512,8 @@ void cleanup_module(void)
 {
 	i2c_del_driver(&driver);
 
-	if ((unregister_chrdev(SCARTSWITCH_MAJOR,"scartswitch"))) {
-			printk("cxa2092.o: unable to release major %d\n", SCARTSWITCH_MAJOR);
+	if ((unregister_chrdev(AVS_MAJOR,"avs"))) {
+			printk("cxa2092.o: unable to release major %d\n", AVS_MAJOR);
 	}
 }
 #endif
--- avs/cxa2092.h
+++ avs/cxa2092.h
@@ -1,5 +1,5 @@
 /*
- *   cxa2092.h - scartswitch driver (dbox-II-project)
+ *   cxa2092.h - audio/video switch driver (dbox-II-project)
  *
  *   Homepage: http://dbox2.elxsi.de
  *
@@ -21,37 +21,177 @@
  *
  *
  *   $Log: cxa2092.h,v $
+ *   Revision 1.3  2001/01/15 17:02:32  gillem
+ *   rewriten
+ *
  *   Revision 1.2  2001/01/06 10:05:43  gillem
  *   cvs check
  *
- *   $Revision: 1.2 $
+ *   $Revision: 1.3 $
  *
  */
 
-#define SCARTSWITCHES		1
+/* WRITE MODE */
 
-#define SCARTSWITCH_SONY     	0
+/* #### DATA 1 #### */
 
-/* outputs */
-#define SCSW_OUT1		0
-#define SCSW_OUT2		1
+/* Elektronik Volume Curse (8dB steps) */
+#define AVS_EVC (7<<5)
+/* Electronik Volume Fine (1dB steps) */
+#define AVS_EVF (7<<2)
+/* TV Audio mute (same Data1&5) */
+#define AVS_MUTE (1<<1)
+/* Zero Cross detector active */
+#define AVS_ZCD 1
 
-/* inputs */
-#define SCSW_INP1		1
-#define SCSW_INP2		2
-#define SCSW_INP3		4
-#define SCSW_INP4		8
-#define SCSW_INP5		16
-#define SCSW_INP6		32
-#define SCSW_INP7		64
-#define SCSW_INP8		128
+/* #### DATA 2 #### */
 
-typedef struct scsw_switch {
-	int out;
-	int inp;
-} scsw_switch;
+/* Video Fast Blanking control */
+#define AVS_FBLK (3<<6)
+/* Select the input video sources for Vout1,Vout2,Vout3,Vout4 */
+#define AVS_VSW1 (7<<3)
+/* Select one of 5 stereo inputs for RTV,LTV */
+#define AVS_ASW1 (7)
 
-/* ioctl's */
-#define IOCTL_WRITE_CONTROL_REG 1
-#define IOCTL_READ_STATUS 			2
-#define IOCTL_SWITCH			      3
+/* #### DATA 3 #### */
+
+/* Video function switch control */
+#define AVS_FNC (3<<6)
+/* Select the input video sources for Vout5,Vout6 */
+#define AVS_VSW2 (7<<3)
+/* Select one of 5 stereo inputs for Rout1,Lout1 */
+#define AVS_ASW2 (7)
+
+/* #### DATA 4 #### */
+
+/* When Y/C Mix=1 Converts Y/C input to CVBS for output through Vout8 */
+
+#define AVS_YCM (1<<7)
+/* Select the input video sources for Vout7 */
+#define AVS_VSW3 (7<<3)
+/* Select one of 5 stereo inputs for Rout2,Lout2 */
+#define AVS_ASW3 (7)
+
+/* #### DATA 5 #### */
+
+/* same as MUTE ! */
+#define AVS_MUTE2 (1<<7)
+/* Logic outputs (open collector) 1=himp. 0=current sink mode */
+#define AVS_LOG (0x0F)
+
+
+/* READ MODE */
+
+/* monitors voltage of pin 8 VCR */
+#define AVS_FVCR (3)
+/* monitors voltage of pin 8 AUX */
+#define AVS_FAUX (3<<2)
+/* POR=1 when DIG_VCC volt. threshold higher than reset level (approx
+5V) */
+#define AVS_POR (1<<4)
+/* ZC Status=1 indicate that zero cross condition has been achieved
+after vol or mute */
+#define AVS_ZCS (1<<5)
+
+/* ### data defines ### */
+
+/* VS1 - TV Output */
+#define AVS_TVOUT_DE1 0
+#define AVS_TVOUT_DE2 1
+#define AVS_TVOUT_VCR 2
+#define AVS_TVOUT_AUX 3
+#define AVS_TVOUT_DE3 4
+#define AVS_TVOUT_DE4 5
+#define AVS_TVOUT_DE5 6
+#define AVS_TVOUT_VM1 7
+
+/* VS2 - VCR Output */
+#define AVS_VCROUT_DE1 0
+#define AVS_VCROUT_DE2 1
+#define AVS_VCROUT_VCR 2
+#define AVS_VCROUT_AUX 3
+#define AVS_VCROUT_DE3 4
+#define AVS_VCROUT_VM1 5
+#define AVS_VCROUT_VM2 6
+#define AVS_VCROUT_VM3 7
+
+/* VS3 - AUX Output */
+#define AVS_AUXOUT_DE1 0
+#define AVS_AUXOUT_VM1 1
+#define AVS_AUXOUT_VCR 2
+#define AVS_AUXOUT_AUX 3
+#define AVS_AUXOUT_DE2 4
+#define AVS_AUXOUT_VM2 5
+#define AVS_AUXOUT_VM3 6
+#define AVS_AUXOUT_VM4 7
+
+/* FBLK */
+#define AVS_FBLKOUT_NULL 0
+#define AVS_FBLKOUT_5V	 1
+#define AVS_FBLKOUT_IN1  2
+#define AVS_FBLKOUT_IN2  3
+
+/* FNC */
+#define AVS_FNCOUT_INTTV		0
+#define AVS_FNCOUT_EXT169		1
+#define AVS_FNCOUT_EXT43		2
+#define AVS_FNCOUT_EXT43_1	3
+
+/* AUDIO Output */
+#define AVS_AUDOUT_V1 0
+#define AVS_AUDOUT_V2 1
+#define AVS_AUDOUT_V3 2
+#define AVS_AUDOUT_V4 3
+#define AVS_AUDOUT_V5 4
+#define AVS_AUDOUT_M1 5
+#define AVS_AUDOUT_M2 6
+#define AVS_AUDOUT_M3 7
+
+/* Volume (course) */
+#define AVS_VOLOUT_C00 0
+#define AVS_VOLOUT_C08 1
+#define AVS_VOLOUT_C16 2
+#define AVS_VOLOUT_C24 3
+#define AVS_VOLOUT_C32 4
+#define AVS_VOLOUT_C40 5
+#define AVS_VOLOUT_C48 6
+#define AVS_VOLOUT_C56 7
+
+/* Volume (fine) */
+#define AVS_VOLOUT_F0 0
+#define AVS_VOLOUT_F1 1
+#define AVS_VOLOUT_F2 2
+#define AVS_VOLOUT_F3 3
+#define AVS_VOLOUT_F4 4
+#define AVS_VOLOUT_F5 5
+#define AVS_VOLOUT_F6 6
+#define AVS_VOLOUT_F7 7
+
+/* Mute */
+#define AVS_MUTE_IM		0
+#define AVS_MUTE_ZC		1
+#define AVS_UNMUTE_IM	2
+#define AVS_UNMUTE_ZC	3
+
+#define SET_EVC(arr,val)	arr[0] = (arr[0]&(~AVS_EVC)) | (val<<5);
+#define SET_EVF(arr,val)	arr[0] = (arr[0]&(~AVS_EVF)) | (val<<2);
+
+/* IOCTL */
+#define AVSIOSVSW1 0
+#define AVSIOSVSW2 1
+#define AVSIOSVSW3 2
+#define AVSIOSASW1 3
+#define AVSIOSASW2 4
+#define AVSIOSASW3 5
+#define AVSIOSVOL  6
+
+#define AVSIOGVSW1 7
+#define AVSIOGVSW2 8
+#define AVSIOGVSW3 9
+#define AVSIOGASW1 10
+#define AVSIOGASW2 11
+#define AVSIOGASW3 12
+#define AVSIOGVOL  13
+
+#define AVSIOSMUTE 14
+#define AVSIOGMUTE 15
