74fafd3dc218fb8c4baafb257df0162317d933ff
gillem <gillem>
2001-02-23 18:43:31 +0000

    - add ioctl's

--- include/dbox/fp.h
+++ include/dbox/fp.h
@@ -1,30 +1,29 @@
 #ifndef __FP_H
 #define __FP_H
 
-#define FP_MAJOR            60
-#define FP_MINOR            0
-#define RC_MINOR            1
-
 #define FP_IOCTL_GETID          0
 #define FP_IOCTL_POWEROFF       1
 #define FP_IOCTL_REBOOT         2
-
-int fp_set_tuner_dword(int type, u32 tw);
-int fp_set_polarisation(int pol);
+#define FP_IOCTL_LCD_DIMM		3
+#define FP_IOCTL_LED			4
 
 #define P_HOR           0
 #define P_VERT          1
-
-int fp_do_reset(int type);
 
 #define T_UNKNOWN       0
 #define T_QAM           1
 #define T_QPSK          2
 
-int fp_send_diseqc(u32 dw);
+#ifdef __KERNEL__
 
+#define FP_MAJOR        60
+#define FP_MINOR        0
+#define RC_MINOR        1
 
- // mehr fallen mir jetzt nicht ein.
+static int fp_set_tuner_dword(int type, u32 tw);
+static int fp_set_polarisation(int pol);
+static int fp_do_reset(int type);
+static int fp_send_diseqc(u32 dw);
 
-
+#endif
 #endif
