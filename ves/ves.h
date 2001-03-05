f01791e862a7bb1122a6ba179bc6ac6f8b5a356f
gillem <gillem>
2001-03-05 18:46:08 +0000

    *** empty log message ***

--- ves/ves.h
+++
@@ -1,9 +0,0 @@
-#ifndef __VES_H
-#define __VES_H
-
-void ves_write_reg(int reg, int val);
-void ves_init(void);
-void ves_set_frontend(struct frontend *front);
-void ves_get_frontend(struct frontend *front);
-
-#endif
