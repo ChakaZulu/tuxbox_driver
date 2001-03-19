bf9e0db05f90ff54c0f955ffb0ef8f6e5c953318
gillem <gillem>
2001-03-19 18:19:58 +0000

    - add function

--- include/dbox/ves.h
+++ include/dbox/ves.h
@@ -5,5 +5,5 @@ void ves_write_reg(int reg, int val);
 void ves_init(void);
 void ves_set_frontend(struct frontend *front);
 void ves_get_frontend(struct frontend *front);
-
+int ves_get_unc_packet(uint32_t *uncp);
 #endif
