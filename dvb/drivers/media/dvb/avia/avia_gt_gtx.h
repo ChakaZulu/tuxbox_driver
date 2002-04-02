7c5cf5f9225a26325d7941c01bc416f4ab8fe2a3
Jolt <Jolt>
2002-04-02 15:05:57 +0000

    PCM defines

--- dvb/drivers/media/dvb/avia/avia_gt_gtx.h
+++ dvb/drivers/media/dvb/avia/avia_gt_gtx.h
@@ -3,6 +3,10 @@
 
 #define GTX_PHYSBASE    0x8000000
 #define GTX_FB_OFFSET	0x0100000
+
+#define GTX_PCM_SIZE            (2 * 2048)
+//#define GTX_PCM_OFFSET                (GTX_MEM_SIZE - GTX_PCM_SIZE)
+#define GTX_PCM_OFFSET          ((1024*1024) - GTX_PCM_SIZE)
                     
 #undef CR0
 #define gRR0            0x100
@@ -123,9 +127,10 @@
 #define GVS_SET_YSZ(X)   rw(GVS) = ((rw(GVS)&(~0x3FF))|(X&0x3FF))
 
 
-
-
-
+#define GTX_REG_PCMA	0x0E0
+#define GTX_REG_PCMN	0x0E4
+#define GTX_REG_PCMC	0x0E8
+#define GTX_REG_PCMD	0x0EC
 #define GTX_REG_VLI1	0x0F8
 #define GTX_REG_RR0	0x100
 #define GTX_REG_VPSA	0x240
@@ -137,7 +142,59 @@
 #define GTX_REG_VCS	0x268
 
 
+#define GTX_IRQ_REG(reg, bit)	(reg)
+#define GTX_IRQ_BIT(reg, bit)	(bit)
 
+#define GTX_IRQ_REG_ISR0	0
+#define GTX_IRQ_REG_ISR1	1
+#define GTX_IRQ_REG_ISR2	2
+#define GTX_IRQ_REG_ISR3	3
+
+#define GTX_IRQ_IR_TX		GTX_IRQ_REG_ISR1, 8
+#define GTX_IRQ_IR_RX		GTX_IRQ_REG_ISR1, 9
+#define GTX_IRQ_PCM_PF		GTX_IRQ_REG_ISR1, 10
+#define GTX_IRQ_PCM_AD		GTX_IRQ_REG_ISR1, 12
+
+
+
+
+typedef struct {
+
+    unsigned short NSAMP: 10;
+    unsigned int Addr: 21;
+    unsigned char W: 1;
+
+} sGTX_REG_PCMA;
+
+typedef struct {
+
+  unsigned char R: 2;
+  unsigned char W: 1;
+  unsigned char C: 1; 
+  unsigned char S: 1; 
+  unsigned char T: 1; 
+  unsigned char V: 1;
+  unsigned char P: 1; 
+  unsigned char M: 1; 
+  unsigned char I: 1; 
+  unsigned char ADV: 2;
+  unsigned char ACD: 2;
+  unsigned char BCD: 2;
+  
+} sGTX_REG_PCMC;
+
+typedef struct {
+
+  unsigned char PCMAL: 7;
+  unsigned char Reserved1: 1;
+  unsigned char PCMAR: 7;
+  unsigned char Reserved2: 1;
+  unsigned char MPEGAL: 7;
+  unsigned char Reserved3: 1;
+  unsigned char MPEGAR: 7;
+  unsigned char Reserved4: 1;
+  
+} sGTX_REG_PCMN;
 
 typedef struct {
 
