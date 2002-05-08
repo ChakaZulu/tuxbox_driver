1b36324008303383b430640df67a0bef5d9f274c
obi <obi>
2002-05-08 13:06:34 +0000

    renamed AUDIO_SPDIF_SET to AUDIO_SET_SPDIF_COPIES renamed corresponding struct moved spdif mute command to AUDIO_SET_MUTE ioctl

--- include/ost/audio.h
+++ include/ost/audio.h
@@ -1,9 +1,9 @@
-/* 
+/*
  * audio.h
  *
  * Copyright (C) 2000 Ralph  Metzler <ralph@convergence.de>
- *                  & Marcus Metzler <marcus@convergence.de>
-                      for convergence integrated media GmbH
+ *		  & Marcus Metzler <marcus@convergence.de>
+		      for convergence integrated media GmbH
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Lesser Public License
@@ -35,53 +35,51 @@
 #define false 0
 
 typedef enum {
-        AUDIO_SOURCE_DEMUX, /* Select the demux as the main source */ 
-	AUDIO_SOURCE_MEMORY /* Select internal memory as the main source */ 
+	AUDIO_SOURCE_DEMUX, /* Select the demux as the main source */
+	AUDIO_SOURCE_MEMORY /* Select internal memory as the main source */
 } audioStreamSource_t;
 
-typedef enum { 
-	AUDIO_STOPPED,      /* Device is stopped */ 
-        AUDIO_PLAYING,      /* Device is currently playing */ 
-	AUDIO_PAUSED        /* Device is paused */ 
+typedef enum {
+	AUDIO_STOPPED,	/* Device is stopped */
+	AUDIO_PLAYING,	/* Device is currently playing */
+	AUDIO_PAUSED	/* Device is paused */
 } audioPlayState_t;
 
 typedef enum {
-        AUDIO_STEREO,
-        AUDIO_MONO_LEFT, 
-	AUDIO_MONO_RIGHT, 
+	AUDIO_STEREO,
+	AUDIO_MONO_LEFT,
+	AUDIO_MONO_RIGHT,
 } audioChannelSelect_t;
 
 typedef enum {
 	SCMS_COPIES_NONE,
 	SCMS_COPIES_ONE,
 	SCMS_COPIES_UNLIMITED,
-	SPDIF_ON,
-	SPDIF_OFF
-} audioSpdifState_t;
+} audioSpdifCopyState_t;
 
-typedef struct audioStatus { 
-        boolean AVSyncState;                /* sync audio and video? */
-        boolean muteState;                  /* audio is muted */ 
-        audioPlayState_t playState;         /* current playback state */
-        audioStreamSource_t streamSource;   /* current stream source */
-        audioChannelSelect_t channelSelect; /* currently selected channel */
-        boolean bypassMode;                 /* pass on audio data to separate 
-					       decoder hardware */
+typedef struct audioStatus {
+	boolean AVSyncState;			/* sync audio and video? */
+	boolean muteState;			/* audio is muted */
+	audioPlayState_t playState;		/* current playback state */
+	audioStreamSource_t streamSource;	/* current stream source */
+	audioChannelSelect_t channelSelect;	/* currently selected channel */
+	boolean bypassMode;			/* pass on audio data to separate decoder hardware */
 } audioStatus_t;
 
-typedef struct audioMixer { 
-        unsigned int volume_left;
-        unsigned int volume_right;
-  // what else do we need? bass, pass-through, ...
+typedef struct audioMixer {
+	unsigned int volume_left;
+	unsigned int volume_right;
+	// what else do we need? bass, pass-through, ...
 } audioMixer_t;
 
 typedef
-struct audioKaraoke{         /* if Vocal1 or Vocal2 are non-zero, they get mixed  */
-	int vocal1;	     /* into left and right t at 70% each */
-	int vocal2;          /* if both, Vocal1 and Vocal2 are non-zero, Vocal1 gets  */
-	int melody;          /* mixed into the left channel and */
-                             /* Vocal2 into the right channel at 100% each. */
-                             /* if Melody is non-zero, the melody channel gets mixed  */                             /* into left and right  */
+struct audioKaraoke {	/* if Vocal1 or Vocal2 are non-zero, they get mixed  */
+	int vocal1;	/* into left and right t at 70% each */
+	int vocal2;	/* if both, Vocal1 and Vocal2 are non-zero, Vocal1 gets  */
+	int melody;	/* mixed into the left channel and */
+			/* Vocal2 into the right channel at 100% each. */
+			/* if Melody is non-zero, the melody channel gets mixed  */
+			/* into left and right  */
 } audioKaraoke_t;
 
 typedef uint16_t audioAttributes_t;
@@ -93,7 +91,7 @@ typedef uint16_t audioAttributes_t;
 /*    7- 6 Quantization / DRC (mpeg audio: 1=DRC exists)(lpcm: 0=16bit,  */
 /*    5- 4 Sample frequency fs (0=48kHz, 1=96kHz) */
 /*    2- 0 number of audio channels (n+1 channels) */
- 
+
 
 /* for GET_CAPABILITIES and SET_FORMAT, the latter should only set one bit */
 #define AUDIO_CAP_DTS    1
@@ -106,27 +104,26 @@ typedef uint16_t audioAttributes_t;
 #define AUDIO_CAP_SDDS 128
 #define AUDIO_CAP_AC3  256
 
-#define AUDIO_STOP                 _IO('o', 1) 
-#define AUDIO_PLAY                 _IO('o', 2)
-#define AUDIO_PAUSE                _IO('o', 3)
-#define AUDIO_CONTINUE             _IO('o', 4)
-#define AUDIO_SELECT_SOURCE        _IOW('o', 5, audioStreamSource_t)
-#define AUDIO_SET_MUTE             _IOW('o', 6, boolean)
-#define AUDIO_SET_AV_SYNC          _IOW('o', 7, boolean)
-#define AUDIO_SET_BYPASS_MODE      _IOW('o', 8, boolean)
-#define AUDIO_CHANNEL_SELECT       _IOW('o', 9, audioChannelSelect_t)
-#define AUDIO_GET_STATUS           _IOR('o', 10, audioStatus_t *)
-
-#define AUDIO_GET_CAPABILITIES     _IOR('o', 11, unsigned int *)
-#define AUDIO_CLEAR_BUFFER         _IO('o',  12)
-#define AUDIO_SET_ID               _IOW('o', 13, int)
-#define AUDIO_SET_MIXER            _IOW('o', 14, audioMixer_t *)
-#define AUDIO_SET_STREAMTYPE       _IOW('o', 15, unsigned int)
-#define AUDIO_SET_EXT_ID           _IOW('o', 16, int)
-#define AUDIO_SET_ATTRIBUTES       _IOW('o', 17, audioAttributes_t)
-#define AUDIO_SET_KARAOKE          _IOW('o', 18, audioKaraoke_t *)
+#define AUDIO_STOP		_IO('o', 1)
+#define AUDIO_PLAY		_IO('o', 2)
+#define AUDIO_PAUSE		_IO('o', 3)
+#define AUDIO_CONTINUE		_IO('o', 4)
+#define AUDIO_SELECT_SOURCE	_IOW('o', 5, audioStreamSource_t)
+#define AUDIO_SET_MUTE		_IOW('o', 6, boolean)
+#define AUDIO_SET_AV_SYNC	_IOW('o', 7, boolean)
+#define AUDIO_SET_BYPASS_MODE	_IOW('o', 8, boolean)
+#define AUDIO_CHANNEL_SELECT	_IOW('o', 9, audioChannelSelect_t)
+#define AUDIO_GET_STATUS	_IOR('o', 10, audioStatus_t *)
+#define AUDIO_GET_CAPABILITIES	_IOR('o', 11, unsigned int *)
+#define AUDIO_CLEAR_BUFFER	_IO('o',  12)
+#define AUDIO_SET_ID		_IOW('o', 13, int)
+#define AUDIO_SET_MIXER		_IOW('o', 14, audioMixer_t *)
+#define AUDIO_SET_STREAMTYPE	_IOW('o', 15, unsigned int)
+#define AUDIO_SET_EXT_ID	_IOW('o', 16, int)
+#define AUDIO_SET_ATTRIBUTES	_IOW('o', 17, audioAttributes_t)
+#define AUDIO_SET_KARAOKE	_IOW('o', 18, audioKaraoke_t *)
 
 /* Tuxbox Linux extensions, not in LinuxTV API */
-#define AUDIO_SPDIF_SET		   _IOW('o', 100, audioSpdifState_t)
+#define AUDIO_SET_SPDIF_COPIES	_IOW('o', 100, audioSpdifCopyState_t)
 
 #endif /* _OST_AUDIO_H_ */
