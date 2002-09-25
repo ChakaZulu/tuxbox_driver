d1e45181374d95e08fb82c7f45a521b6a25c48e3
obi <obi>
2002-09-25 21:08:07 +0000

    build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# example driver build script
+# modify to suit your setup
+#
+# usage:
+#    ./build.sh clean
+#    ./build.sh
+#    ./build.sh install
+#
+# can be called from any subdirectory
+#
+
+CDKROOT=/dbox2/cdkroot
+CVSROOT=${HOME}/tuxbox-cvs
+KERNEL_VERSION=2.4.19
+
+make KERNEL_LOCATION=${CVSROOT}/cdk/linux-${KERNEL_VERSION} MODULE_DEST=${CDKROOT}/lib/modules/${KERNEL_VERSION}/misc ${1}
+
