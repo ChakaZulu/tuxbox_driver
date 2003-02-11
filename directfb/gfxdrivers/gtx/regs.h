a4d25cda2692b71de5e2c13752c270195df0a26a
obi <obi>
2003-02-11 01:06:56 +0000

    eNX register definitions

--- directfb/gfxdrivers/gtx/regs.h
+++ directfb/gfxdrivers/gtx/regs.h
@@ -1,19 +1,25 @@
 #ifndef REGS_H
 #define REGS_H
 
-#define ENX_TCR1	0x03C8
+#define ENX_TCR1  0x03C8 /* Transparent Color Register */
+#define ENX_BDST  0x0580 /* Blitter Destination Register */
+#define ENX_BMR   0x0584 /* Blitter Mask Register */
+#define ENX_BDR   0x0588 /* Blitter Data Register */
+#define ENX_BPW   0x058C /* Blitter Pixel Width Register */
+#define ENX_BPO   0x058E /* Blitter Pixel Offset Register */
+#define ENX_GCSRC 0x05C0 /* Graphics Copy Source Address Register */
+#define ENX_GCDST 0x05C4 /* Graphics Copy Destination Address Register */
+#define ENX_GCCMD 0x05C8 /* Graphics Copy Command Register */
 
-#define GTX_TCR  0x08
-#define GTX_BDST 0xB0
-#define GTX_BMR  0xB4
-#define GTX_BDR  0xB6
-#define GTX_BCLR 0xB8
-#define GTX_BPW  0xBC
-#define GTX_BPO  0xBE
-
-#define GTX_CSA  0x220
-#define GTX_CDA  0x224
-#define GTX_CCOM 0x228
+#define GTX_TCR   0x0008 /* Transparent Color Register */
+#define GTX_BDST  0x00B0 /* Blitter Destination Register */
+#define GTX_BMR   0x00B4 /* Blitter Mask Register */
+#define GTX_BDR   0x00B6 /* Blitter Data Register */
+#define GTX_BCLR  0x00B8 /* Blitter Color Register */
+#define GTX_BPW   0x00BC /* Blitter Pixel Width Register */
+#define GTX_BPO   0x00BE /* Blitter Pixel Offset Register */
+#define GTX_CSA   0x0220 /* Graphics Copy Source Address Register */
+#define GTX_CDA   0x0224 /* Graphics Copy Destination Address Register */
+#define GTX_CCOM  0x0228 /* Graphics Copy Command Register Register */
 
 #endif
-
