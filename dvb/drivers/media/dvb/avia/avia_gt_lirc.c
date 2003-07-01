072dd26d465dec2a323fb7020290b3de476baff7
obi <obi>
2003-07-01 12:45:12 +0000

    source layout

--- dvb/drivers/media/dvb/avia/avia_gt_lirc.c
+++ dvb/drivers/media/dvb/avia/avia_gt_lirc.c
@@ -1,5 +1,5 @@
 /*
- * $Id: avia_gt_lirc.c,v 1.9 2003/07/01 04:06:17 obi Exp $
+ * $Id: avia_gt_lirc.c,v 1.10 2003/07/01 12:45:12 obi Exp $
  *
  * lirc ir driver for AViA eNX/GTX (dbox-II-project)
  *
@@ -54,102 +54,60 @@ static lirc_t pulse_buffer[AVIA_GT_IR_MAX_PULSE_COUNT * 2];
 
 static int avia_gt_lirc_ioctl(struct inode *inode, struct file *file, unsigned int cmd, unsigned long arg)
 {
+	unsigned long value = 0;
+	int result = 0;
 
-	unsigned long	 value	= (unsigned long)0;
-	int						 result	= (int)0;
+	switch (cmd) {
+	case LIRC_GET_FEATURES:
+		return put_user(LIRC_CAN_REC_MODE2 | LIRC_CAN_SEND_PULSE | LIRC_CAN_SET_SEND_CARRIER | LIRC_CAN_SET_SEND_DUTY_CYCLE, (unsigned long *)arg);
 
-	switch(cmd) {
+	case LIRC_GET_REC_MODE:
+		return put_user(LIRC_MODE_MODE2, (unsigned long *)arg);
 
-		case LIRC_GET_FEATURES:
+	case LIRC_GET_SEND_MODE:
+		return put_user(LIRC_MODE_PULSE, (unsigned long *)arg);
 
-			result = put_user(LIRC_CAN_REC_MODE2 | LIRC_CAN_SEND_PULSE | LIRC_CAN_SET_SEND_CARRIER | LIRC_CAN_SET_SEND_DUTY_CYCLE, (unsigned long *)arg);
-
-			if (result)
-				return result;
-
+	case LIRC_SET_REC_MODE:
+		result = get_user(value, (unsigned long *)arg);
+		if (result)
+			return result;
+		if (value != LIRC_MODE_MODE2)
+			return -EINVAL;
 		break;
 
-		case LIRC_GET_REC_MODE:
-
-			result = put_user(LIRC_MODE_MODE2, (unsigned long *)arg);
-
-			if (result)
-				return result;
-
+	case LIRC_SET_SEND_CARRIER:
+		result = get_user(value, (unsigned long *)arg);
+		if (result)
+			return result;
+		avia_gt_ir_set_frequency(value);
 		break;
 
-		case LIRC_GET_SEND_MODE:
-
-			result = put_user(LIRC_MODE_PULSE, (unsigned long *)arg);
-
-			if (result)
-				return result;
-
+	case LIRC_SET_SEND_DUTY_CYCLE:
+		result = get_user(value, (unsigned long *)arg);
+		if (result)
+			return result;
+		avia_gt_ir_set_duty_cycle(value);
 		break;
 
-		case LIRC_SET_REC_MODE:
-
-			result = get_user(value, (unsigned long *)arg);
-
-			if (result)
-				return result;
-
-			if (value != LIRC_MODE_MODE2)
-				return -EINVAL;
-
+	case LIRC_SET_SEND_MODE:
+		result = get_user(value, (unsigned long *)arg);
+		if (result)
+			return result;
+		if (value != LIRC_MODE_PULSE)
+			return -EINVAL;
 		break;
 
-		case LIRC_SET_SEND_CARRIER:
-
-			result = get_user(value, (unsigned long *)arg);
-
-			if (result)
-				return result;
-
-			avia_gt_ir_set_frequency(value);
-
-		break;
-
-		case LIRC_SET_SEND_DUTY_CYCLE:
-
-			result = get_user(value, (unsigned long *)arg);
-
-			if (result)
-				return result;
-
-			avia_gt_ir_set_duty_cycle(value);
-
-		break;
-
-		case LIRC_SET_SEND_MODE:
-
-			result = get_user(value, (unsigned long *)arg);
-
-			if (result)
-				return result;
-
-			if (value != LIRC_MODE_PULSE)
-				return -EINVAL;
-
-		break;
-
-		default:
-
-			return -ENOIOCTLCMD;
-
-		break;
-
+	default:
+		return -ENOIOCTLCMD;
 	}
 
 	return 0;
-
 }
 
 static u8 got_pulse = 0;
 
 static unsigned int avia_gt_lirc_poll(struct file *file, poll_table *wait)
 {
-
 	if ((avia_gt_ir_get_rx_buffer_read_position() == avia_gt_ir_get_rx_buffer_write_position()) && (!got_pulse))
 		poll_wait(file, avia_gt_ir_receive_data(), wait);
 
@@ -157,7 +115,6 @@ static unsigned int avia_gt_lirc_poll(struct file *file, poll_table *wait)
 		return (POLLIN | POLLRDNORM);
 	else
 		return 0;
-
 }
 					
 static u32 pulse_len = 0;
@@ -167,7 +124,6 @@ static u32 pulse_len = 0;
 
 static ssize_t avia_gt_lirc_read(struct file *file, char *buf, size_t count, loff_t *f_pos)
 {
-
 	u32 period_high = 0;
 	u32 period_low = 0;
 //	u32 rand_val;
@@ -181,13 +137,10 @@ static ssize_t avia_gt_lirc_read(struct file *file, char *buf, size_t count, lof
 	while ((done * sizeof(lirc_t)) < count) {
 	
 		if (got_pulse) {
-		
 			rx_buffer[done] = pulse_len | PULSE_BIT;
-		
 			got_pulse = 0;
-
-		} else {
-
+		}
+		else {
 /*			if (got_next) {
 			
 				period_low = next_low;
@@ -196,10 +149,8 @@ static ssize_t avia_gt_lirc_read(struct file *file, char *buf, size_t count, lof
 				got_next = 0;
 			
 			} else {*/
-			
 				if ((result = avia_gt_ir_receive_pulse(&period_low, &period_high, !(file->f_flags & O_NONBLOCK))))
 					return result;
-					
 //			}
 
 /*			if ((period_high > 350) && (period_high < 750))
@@ -261,24 +212,20 @@ static ssize_t avia_gt_lirc_read(struct file *file, char *buf, size_t count, lof
 				rx_buffer[done] = PULSE_MASK;
 			else
 				rx_buffer[done] = period_low;
-
 		}
 
 		done++;
-
 	}
 
 	return (done * sizeof(lirc_t));
-
 }
 
 static ssize_t avia_gt_lirc_write(struct file *file, const char *buf, size_t count, loff_t *offset)
 {
-
 	u32 pulse_count = 0;
 	u32 pulse_nr = 0;
 	int result = 0;
-	
+
 	if (count % sizeof(lirc_t))
 		return -EINVAL;
 
@@ -290,57 +237,43 @@ static ssize_t avia_gt_lirc_write(struct file *file, const char *buf, size_t cou
 
 	pulse_count = count / sizeof(lirc_t);
 
-	for (pulse_nr = 0; pulse_nr < pulse_count; pulse_nr += 2) {
-
+	for (pulse_nr = 0; pulse_nr < pulse_count; pulse_nr += 2)
 		if ((result = avia_gt_ir_queue_pulse(pulse_buffer[pulse_nr], pulse_buffer[pulse_nr + 1], !(file->f_flags & O_NONBLOCK))))
 			return result;
 
-	}
-
 	return avia_gt_ir_send_buffer(!(file->f_flags & O_NONBLOCK));
-
 }
 
 static struct file_operations avia_gt_lirc_fops = {
-
 	.owner = THIS_MODULE,
 	.ioctl = avia_gt_lirc_ioctl,
 	.poll = avia_gt_lirc_poll,
 	.read = avia_gt_lirc_read,
 	.write = avia_gt_lirc_write,
-
 };
 
 static int __init avia_gt_lirc_init(void)
 {
-
-	printk("avia_gt_lirc: $Id: avia_gt_lirc.c,v 1.9 2003/07/01 04:06:17 obi Exp $\n");
+	printk(KERN_INFO "avia_gt_lirc: $Id: avia_gt_lirc.c,v 1.10 2003/07/01 12:45:12 obi Exp $\n");
 
 	if (avia_gt_ir_init() < 0)
-	{
 		return -EIO;
-	}
 
 	devfs_handle = devfs_register(NULL, "lirc", DEVFS_FL_DEFAULT, 0, 0, S_IFCHR | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH, &avia_gt_lirc_fops, NULL);
 
 	if (!devfs_handle) {
-
-		printk("avia_gt_lirc: error - can't register devfs handle\n");
-
+		printk(KERN_ERR "avia_gt_lirc: devfs_register failed\n");
 		return -EIO;
-
 	}
 
 	avia_gt_ir_set_frequency(38000);
 	avia_gt_ir_set_duty_cycle(33);
 
 	return 0;
-
 }
 
 static void __exit avia_gt_lirc_exit(void)
 {
-
 	devfs_unregister (devfs_handle);
 	avia_gt_ir_exit();
 }
@@ -351,4 +284,3 @@ module_exit(avia_gt_lirc_exit);
 MODULE_AUTHOR("Florian Schirmer <jolt@tuxbox.org>");
 MODULE_DESCRIPTION("AViA eNX/GTX LIRC driver");
 MODULE_LICENSE("GPL");
-
