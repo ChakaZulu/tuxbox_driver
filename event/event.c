8257e2015701021e9ef772ea2f8ada558512dc77
obi <obi>
2003-09-08 22:38:56 +0000

    coding style "fixes"

--- event/event.c
+++ event/event.c
@@ -1,81 +1,36 @@
 /*
- *	event.c - global event driver (dbox-II-project)
+ * $Id: event.c,v 1.11 2003/09/08 22:38:56 obi Exp $
+ * 
+ * global event driver (dbox-II-project)
  *
- *	Homepage: http://dbox2.elxsi.de
+ * Homepage: http://www.tuxbox.org
  *
- *	Copyright (C) 2001 Gillem (gillem@berlios.de)
+ * Copyright (C) 2001 Gillem <gillem@berlios.de>
  *
- *	This program is free software; you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation; either version 2 of the License, or
- *	(at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *	You should have received a copy of the GNU General Public License
- *	along with this program; if not, write to the Free Software
- *	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- *	$Log: event.c,v $
- *	Revision 1.10  2002/11/25 11:02:25  obi
- *	clean up kernel symbols
- *	
- *	Revision 1.9  2002/08/12 17:00:47  obi
- *	removed compiler warning
- *	
- *	Revision 1.8  2002/05/06 02:18:19  obi
- *	cleanup for new kernel
- *	
- *	Revision 1.7  2002/03/03 13:58:39  gillem
- *	- handle more events
- *
- *	Revision 1.6  2002/03/02 17:03:58  waldi
- *	merge new_tuning_api
- *
- *	Revision 1.5.2.1  2002/03/02 16:53:03  tmbinc
- *	added poll()-support to event device
- *
- *	Revision 1.5  2001/12/19 21:15:20  gillem
- *	- more work on event stuff ...
- *
- *	Revision 1.4  2001/12/19 19:47:01  gillem
- *	- some work on event-filter
- *
- *	Revision 1.3  2001/12/08 15:20:58  gillem
- *	- remove debug stuff ;-)
- *
- *	Revision 1.2  2001/12/08 15:13:54  gillem
- *	- add more functions
- *
- *	Revision 1.1  2001/12/07 14:30:37  gillem
- *	- initial release (not ready today)
- *
- *
- *	$Revision: 1.10 $
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  */
 
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/sched.h>
-#include <linux/string.h>
-#include <linux/timer.h>
-#include <linux/delay.h>
-#include <linux/errno.h>
-#include <linux/slab.h>
-#include <linux/poll.h>
-#include <linux/types.h>
-#include <linux/slab.h>
 #include <linux/devfs_fs_kernel.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/poll.h>
+#include <linux/slab.h>
 
 #include <dbox/event.h>
-
-#ifndef CONFIG_DEVFS_FS
-#error no devfs
-#endif
 
 #define EVENTBUFFERSIZE		32
 
@@ -93,107 +48,78 @@ struct event_private_t {
 
 #define MAX_EVENT_OPEN 5
 
-static int open_handle;
-static struct event_private_t * event_private[5];
-
+static int open_handle = 0;
+static struct event_private_t *event_private[5];
 static DECLARE_WAIT_QUEUE_HEAD(event_wait);
-
 static spinlock_t event_lock;
-
 static devfs_handle_t devfs_handle;
 
-static int event_ioctl (struct inode *inode, struct file *file,
-			 unsigned int cmd, unsigned long arg);
-static int event_open (struct inode *inode, struct file *file);
-static int event_release (struct inode *inode, struct file *file);
-static unsigned int event_poll(struct file *file, poll_table *wait);
+#define dprintk(x...)
+//#define dprintk(x...) printk(x)
 
-static ssize_t event_read (struct file *file, char *buf, size_t count, loff_t *offset);
-
-static struct file_operations event_fops = {
-	owner:    THIS_MODULE,
-	read:     event_read,
-	ioctl:    event_ioctl,
-	open:     event_open,
-	release:  event_release,
-	poll:     event_poll
-};
-
-#define dprintk     if (debug) printk
-
-static int debug = 0;
-
-int event_write_message( struct event_t * event, size_t count )
+void event_write_message(struct event_t *event, size_t count)
 {
-	int i,s;
+	struct event_private_t *e;
+	int i, s;
 
-	spin_lock (&event_lock);
+	spin_lock(&event_lock);
 
-	printk("write %d event's ...\n",count);
+	dprintk("write %d event's ...\n", count);
 
-	for(s=0;s<count;s++)
-	{
-		for(i=0;i<MAX_EVENT_OPEN;i++)
-		{
-			if(event_private[i])
-			{
-				printk("write event ... free found\n");
-
-				if ( (event_private[i]->event_data.event_free > 0) && (event[s].event&event_private[i]->event_filter) )
-				{
-					printk("write event ... filter ok\n");
-					event_private[i]->event_data.event_free--;
-					memcpy( &event_private[i]->event_data.event[event_private[i]->event_data.event_ptr], &event[s], sizeof(struct event_t) );
-					event_private[i]->event_data.event_ptr++;
-					if ( EVENTBUFFERSIZE == event_private[i]->event_data.event_ptr )
-						event_private[i]->event_data.event_ptr = 0;
+	for (s = 0; s < count; s++) {
+		for (i = 0; i < MAX_EVENT_OPEN; i++) {
+			if (event_private[i]) {
+				e = event_private[i];
+				dprintk("write event ... free found\n");
+				if ((e->event_data.event_free > 0) && (event[s].event & e->event_filter)) {
+					dprintk("write event ... filter ok\n");
+					e->event_data.event_free--;
+					memcpy(&e->event_data.event[e->event_data.event_ptr], &event[s], sizeof(struct event_t));
+					e->event_data.event_ptr++;
+					if (EVENTBUFFERSIZE == e->event_data.event_ptr)
+						e->event_data.event_ptr = 0;
 				}
 			}
 		}
 	}
 
-	spin_unlock (&event_lock);
+	spin_unlock(&event_lock);
 
 	wake_up_interruptible(&event_wait);
-
-	return 0;
 }
 
-static int event_ioctl (struct inode *inode, struct file *file, unsigned int cmd,
+static int event_ioctl(struct inode *inode, struct file *file, unsigned int cmd,
 		  unsigned long arg)
 {
-	struct event_private_t * event_priv;
+	struct event_private_t *event_priv = file->private_data;
 
-	event_priv = (struct event_private_t*)file->private_data;
-
-	switch(cmd)
-	{
-		case EVENT_SET_FILTER:
-			printk("set event: %08lX\n",arg);
-			event_priv->event_filter = arg;
-			break;
-		default:
-			break;
+	switch (cmd) {
+	case EVENT_SET_FILTER:
+		dprintk("set event: %08lX\n",arg);
+		event_priv->event_filter = arg;
+		break;
+	default:
+		break;
 	}
 
 	return 0;
 }
 
-static int event_open (struct inode *inode, struct file *file)
+static int event_open(struct inode *inode, struct file *file)
 {
 	int i;
 
-	if(open_handle==MAX_EVENT_OPEN)
-		return -1; //???
+	if (open_handle == MAX_EVENT_OPEN)
+		return -EMFILE;
 
 	open_handle++;
-	file->private_data = kmalloc( sizeof(struct event_private_t), GFP_KERNEL );
 
-	memset(file->private_data,0,sizeof(struct event_private_t));
+	file->private_data = kmalloc(sizeof(struct event_private_t), GFP_KERNEL);
+	memset(file->private_data, 0 ,sizeof(struct event_private_t));
 
-	for(i=0;i<MAX_EVENT_OPEN;i++)
-		if(event_private[i]==0) {
-			event_private[i]=file->private_data;
+	for (i = 0; i < MAX_EVENT_OPEN; i++)
+		if (event_private[i] == NULL) {
+			event_private[i] = file->private_data;
 			break;
 		}
 
@@ -203,30 +129,30 @@ static int event_open (struct inode *inode, struct file *file)
 	return 0;
 }
 
-static int event_release (struct inode *inode, struct file *file)
+static int event_release(struct inode *inode, struct file *file)
 {
 	int i;
 	open_handle--;
 
-	if(file->private_data)
-	{
-		for(i=0;i<MAX_EVENT_OPEN;i++)
-			if(event_private[i]==file->private_data) {
-				event_private[i]=0;
+	if (file->private_data) {
+		for(i = 0; i < MAX_EVENT_OPEN; i++)
+			if (event_private[i] == file->private_data) {
+				event_private[i] = NULL;
 				break;
 			}
 		kfree(file->private_data);
-		file->private_data = (void*) 0;
+		file->private_data = NULL;
 	}
+
 	return 0;
 }
 
-static ssize_t event_read (struct file *file, char *buf, size_t count, loff_t *offset)
+static ssize_t event_read(struct file *file, char *buf, size_t count, loff_t *offset)
 {
+	struct event_private_t *event_priv = file->private_data;
 	DECLARE_WAITQUEUE(wait, current);
 	ssize_t retval = 0;
-	int err = 0;
-	struct event_private_t * event_priv;
+	int err;
 
 	if (count < sizeof(struct event_t))
 		return -EINVAL;
@@ -235,14 +161,12 @@ static ssize_t event_read (struct file *file, char *buf, size_t count, loff_t *o
 
 	current->state = TASK_INTERRUPTIBLE;
 
-	event_priv = (struct event_private_t*)file->private_data;
-
 	do {
 		err = 0;
 
-		spin_lock_irq (&event_lock);
+		spin_lock_irq(&event_lock);
 
-		if ( EVENTBUFFERSIZE != event_priv->event_data.event_free ) {
+		if (EVENTBUFFERSIZE != event_priv->event_data.event_free) {
 			dprintk("found data %d %d\n",event_priv->event_data.event_free,event_priv->event_data.event_read_ptr);
 			retval = put_user(event_priv->event_data.event[event_priv->event_data.event_read_ptr], (struct event_t *)buf);
 
@@ -250,18 +174,17 @@ static ssize_t event_read (struct file *file, char *buf, size_t count, loff_t *o
 				event_priv->event_data.event_free++;
 
 				event_priv->event_data.event_read_ptr++;
-				if ( EVENTBUFFERSIZE == event_priv->event_data.event_read_ptr )
+				if (EVENTBUFFERSIZE == event_priv->event_data.event_read_ptr)
 					event_priv->event_data.event_read_ptr = 0;
 
 				retval = sizeof(unsigned long);
 			}
-			else
+			else {
 				err = 1;
+			}
 		}
-		else
-			dprintk("no data !\n");
 
-		spin_unlock_irq (&event_lock);
+		spin_unlock_irq(&event_lock);
 
 		if (err || retval)
 			break;
@@ -270,11 +193,14 @@ static ssize_t event_read (struct file *file, char *buf, size_t count, loff_t *o
 			retval = -EAGAIN;
 			break;
 		}
+		
 		if (signal_pending(current)) {
 			retval = -ERESTARTSYS;
 			break;
 		}
+
 		schedule();
+
 	} while (1);
 
 	current->state = TASK_RUNNING;
@@ -284,54 +210,54 @@ static ssize_t event_read (struct file *file, char *buf, size_t count, loff_t *o
 	return retval;
 }
 
-unsigned int event_poll(struct file *file, poll_table *wait)
+static unsigned int event_poll(struct file *file, poll_table *wait)
 {
-	struct event_private_t * event_priv;
-
-	event_priv = (struct event_private_t*)file->private_data;
+	struct event_private_t *event_priv = file->private_data;
 
 	poll_wait(file, &event_wait, wait);
+
 	if (EVENTBUFFERSIZE != event_priv->event_data.event_free)
-		return POLLIN|POLLRDNORM;
+		return (POLLIN | POLLRDNORM);
+
 	return 0;
-	return -EINVAL;
 }
 
+static struct file_operations event_fops = {
+	.owner = THIS_MODULE,
+	.read = event_read,
+	.ioctl = event_ioctl,
+	.open = event_open,
+	.release = event_release,
+	.poll = event_poll,
+};
 
-#ifdef MODULE
-int init_module(void)
-#else
-int event_init(void)
-#endif
+static int __init event_init(void)
 {
-	printk("event: init ...\n");
+	printk(KERN_INFO "event: $Id: event.c,v 1.11 2003/09/08 22:38:56 obi Exp $\n");
 
-	open_handle = 0;
+	memset(event_private, 0, sizeof(event_private));
 
-	memset(event_private,0,sizeof(event_private));
-
-	devfs_handle = devfs_register ( NULL, "dbox/event0", DEVFS_FL_DEFAULT,
+	devfs_handle = devfs_register( NULL, "dbox/event0", DEVFS_FL_DEFAULT,
 		0, 0,
 		S_IFCHR | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH,
-		&event_fops, NULL );
+		&event_fops, NULL);
 
-	if ( ! devfs_handle )
-	{
+	if (!devfs_handle)
 		return -EIO;
-	}
 
 	return 0;
 }
 
-#ifdef MODULE
-void cleanup_module(void)
+static void __exit event_exit(void)
 {
-	printk("event: cleanup ...\n");
-
-	devfs_unregister ( devfs_handle );
+	devfs_unregister(devfs_handle);
 }
 
-#ifdef MODULE_LICENSE
+module_init(event_init);
+module_exit(event_exit);
+
+MODULE_AUTHOR("Gillem <gillem@berlios.de>");
+MODULE_DESCRIPTION("global event driver");
 MODULE_LICENSE("GPL");
-#endif
-#endif
+
+EXPORT_SYMBOL(event_write_message);
--- include/dbox/event.h
+++ include/dbox/event.h
@@ -1,46 +1,25 @@
 /*
- *	event.h - global event driver (dbox-II-project)
+ * $Id: event.h,v 1.7 2003/09/08 22:38:57 obi Exp $
  *
- *	Homepage: http://dbox2.elxsi.de
+ * global event driver (dbox-II-project)
  *
- *	Copyright (C) 2001 Gillem (gillem@berlios.de)
+ * Homepage: http://www.tuxbox.org
  *
- *	This program is free software; you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation; either version 2 of the License, or
- *	(at your option) any later version.
+ * Copyright (C) 2001 Gillem (gillem@berlios.de)
  *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *	You should have received a copy of the GNU General Public License
- *	along with this program; if not, write to the Free Software
- *	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *	$Log: event.h,v $
- *	Revision 1.6  2002/11/25 11:02:25  obi
- *	clean up kernel symbols
- *	
- *	Revision 1.5  2002/03/02 18:33:13  tmbinc
- *	changed VCR_ON/OFF to _CHANGED, added IOCTL to get status
- *	
- *	Revision 1.4  2002/02/28 20:41:46  gillem
- *	- add vcr/tv slow blanking event
- *	
- *	Revision 1.3  2001/12/19 19:46:47  gillem
- *	- some work on event-filter
- *	
- *	Revision 1.2  2001/12/18 17:58:52  gillem
- *	- add events
- *	- TODO: filter events
- *	
- *	Revision 1.1  2001/12/08 15:12:33  gillem
- *	- initial release
- *	
- *
- *	$Revision: 1.6 $
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  */
 
@@ -50,7 +29,7 @@
 /* global event defines
  */
 #define EVENT_NOP		0
-#define EVENT_VCR_CHANGED		1
+#define EVENT_VCR_CHANGED	1
 #define EVENT_VHSIZE_CHANGE	4
 #define EVENT_ARATIO_CHANGE	8
 #define EVENT_SBTV_CHANGE	16	/* avs event pin 8 tv */
@@ -66,7 +45,7 @@ struct event_t {
 };
 
 #ifdef __KERNEL__
-extern int event_write_message( struct event_t * buf, size_t count );
+void event_write_message(struct event_t *buf, size_t count);
 #endif
 
 #endif
