363d3f68629048522e8fc536c678b9e8a428e40c
obi <obi>
2003-11-20 23:00:56 +0000

    fixed old nokia rc (use break codes instead of timer)

--- fp/dbox2_fp_rc.c
+++ fp/dbox2_fp_rc.c
@@ -1,5 +1,5 @@
 /*
- * $Id: dbox2_fp_rc.c,v 1.17 2003/11/20 01:12:02 carjay Exp $
+ * $Id: dbox2_fp_rc.c,v 1.18 2003/11/20 23:00:56 obi Exp $
  *
  * Copyright (C) 2002 by Florian Schirmer <jolt@tuxbox.org>
  *
@@ -19,14 +19,9 @@
  *
  */
 
-#include <linux/module.h>
 #include <linux/init.h>
-#include <linux/devfs_fs_kernel.h>
-#include <linux/wait.h>
-#include <linux/slab.h>
-#include <linux/tqueue.h>
-#include <linux/poll.h>
-#include <linux/input.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
 
 #include <dbox/dbox2_fp_core.h>
 
@@ -42,19 +37,15 @@ enum {
 static struct input_dev *rc_input_dev;
 static int old_rc = 1;
 static int new_rc = 1;
-static u8 toggle_bits = 0xFF;
 
 //#define dprintk printk
 #define dprintk if (0) printk
 
-static struct {
-
+static struct rc_key {
 	unsigned long code;
 	u8 value_new;
 	u8 value_old;
-	
 } rc_key_map[] = {
-
 	{KEY_0,				0x00, 0x00},
 	{KEY_1,				0x01, 0x01},
 	{KEY_2,				0x02, 0x02},
@@ -87,122 +78,128 @@ static struct {
 	{KEY_HOME,			0x1F, 0x20},
 	{KEY_PAGEDOWN,			0x53, 0x53},
 	{KEY_PAGEUP,			0x54, 0x54},
-	
 };
 
-#define RC_KEY_COUNT	(sizeof(rc_key_map) / sizeof(rc_key_map[0]))
+#define RC_KEY_COUNT	ARRAY_SIZE(rc_key_map)
 
 static void dbox2_fp_rc_keyup(unsigned long data)
 {
-
 	if ((!data) || (!test_bit(data, rc_input_dev->key)))
 		return;
 
 	input_event(rc_input_dev, EV_KEY, data, KEY_RELEASED);	// "key released" event after timeout
-
 }
 
 static struct timer_list keyup_timer = { 
-
 	.function = dbox2_fp_rc_keyup
-	
 };
 
-void dbox2_fp_rc_queue_handler(u8 queue_nr)
+static void dbox2_fp_old_rc_queue_handler(u8 queue_nr)
 {
-
+	static struct rc_key *last_key = NULL;
+	struct rc_key *key;
 	u16 rc_code;
-	u8 rc_key_nr;
 
-	dprintk("event on queue %d\n", queue_nr);
+	fp_cmd(fp_get_i2c(), 0x01, (u8*)&rc_code, sizeof(rc_code));
 
-	if ((queue_nr != 0) && (queue_nr != 3))
+	if (rc_code == 0x5cfe) {
+		if (last_key) {
+			input_event(rc_input_dev, EV_KEY, last_key->code, KEY_RELEASED);
+			last_key = NULL;
+		}
 		return;
-
-	u8 cmd;
-	switch (mid) {
-		case TUXBOX_DBOX2_MID_NOKIA:
-			cmd = 0x01;		
-			break;
-			
-		case TUXBOX_DBOX2_MID_PHILIPS:
-		case TUXBOX_DBOX2_MID_SAGEM:
-			cmd = 0x26;	
-			break;
 	}
+
+	for (key = rc_key_map; key < &rc_key_map[RC_KEY_COUNT]; key++) {
+		if (key->value_old == (rc_code & 0xff)) {
+			if ((last_key) && (last_key->code == key->code)) {
+				input_event(rc_input_dev, EV_KEY, key->code, KEY_AUTOREPEAT);
+				break;
+			}
+			else {
+				input_event(rc_input_dev, EV_KEY, key->code, KEY_PRESSED);
+				last_key = key;
+				break;
+			}
+		}
+	}
+}
+
+static void dbox2_fp_new_rc_queue_handler(u8 queue_nr)
+{
+	static u8 toggle_bits = 0xff;
+	struct rc_key *key;
+	u16 rc_code;
+	u8 cmd;
+
+	switch (mid) {
+	case TUXBOX_DBOX2_MID_NOKIA:
+		cmd = 0x01;
+		break;
+	case TUXBOX_DBOX2_MID_PHILIPS:
+	case TUXBOX_DBOX2_MID_SAGEM:
+		cmd = 0x26;
+		break;
+	}
+
 	fp_cmd(fp_get_i2c(), cmd, (u8*)&rc_code, sizeof(rc_code));
 
-	dprintk("raw=0x%08X code=0x%08X rest=0x%08X\n", rc_code, rc_code & 0x1F, rc_code & (~0x1F));
-
-	for (rc_key_nr = 0; rc_key_nr < RC_KEY_COUNT; rc_key_nr++) {
-	
-		if (((queue_nr == 0) && (rc_key_map[rc_key_nr].value_old == (rc_code & 0xFF))) ||
-			((queue_nr == 3) && (rc_key_map[rc_key_nr].value_new == (rc_code & 0x1F)))) {
-		
+	for (key = rc_key_map; key < &rc_key_map[RC_KEY_COUNT]; key++) {
+		if (key->value_new == (rc_code & 0x1f)) {
 			if (timer_pending(&keyup_timer)) {
-	
 				del_timer(&keyup_timer);
-	
-				if ((keyup_timer.data != rc_key_map[rc_key_nr].code) || (toggle_bits != ((rc_code >> 6) & 0x03)))
-					input_event(rc_input_dev, EV_KEY, keyup_timer.data, KEY_RELEASED);	// "key released" event because a new key arrived
+				if ((keyup_timer.data != key->code) || (toggle_bits != ((rc_code >> 6) & 0x03)))
+					input_event(rc_input_dev, EV_KEY, keyup_timer.data, KEY_RELEASED);
 			}
-																		
-			if (toggle_bits == ((rc_code>>6)&0x03))
-				input_event(rc_input_dev, EV_KEY, rc_key_map[rc_key_nr].code, KEY_AUTOREPEAT);		// "key autorepeat" event
+
+			if (toggle_bits == ((rc_code >> 6) & 0x03))
+				input_event(rc_input_dev, EV_KEY, key->code, KEY_AUTOREPEAT);
 			else
-				input_event(rc_input_dev, EV_KEY, rc_key_map[rc_key_nr].code, KEY_PRESSED);		// "key pressed" event
+				input_event(rc_input_dev, EV_KEY, key->code, KEY_PRESSED);
 
 			keyup_timer.expires = jiffies + UP_TIMEOUT;
-			keyup_timer.data = rc_key_map[rc_key_nr].code;
+			keyup_timer.data = key->code;
 
 			add_timer(&keyup_timer);
-			
+
 			toggle_bits = (rc_code >> 6) & 0x03;
 		}
-
 	}
-
 }
 
 int __init dbox2_fp_rc_init(struct input_dev *input_dev)
 {
+	struct rc_key *key;
 
-	u8 rc_key_nr;
-	
 	rc_input_dev = input_dev;
-		
+
 	set_bit(EV_KEY, rc_input_dev->evbit);
 
-	for (rc_key_nr = 0; rc_key_nr < RC_KEY_COUNT; rc_key_nr++)
-		set_bit(rc_key_map[rc_key_nr].code, rc_input_dev->keybit);
+	for (key = rc_key_map; key < &rc_key_map[RC_KEY_COUNT]; key++)
+		set_bit(key->code, rc_input_dev->keybit);
 
 	if (old_rc)
-		dbox2_fp_queue_alloc(0, dbox2_fp_rc_queue_handler);
+		dbox2_fp_queue_alloc(0, dbox2_fp_old_rc_queue_handler);
 
 	if (new_rc)
-		dbox2_fp_queue_alloc(3, dbox2_fp_rc_queue_handler);
-		
-	// Enable break codes	
-	//fp_sendcmd(fp_get_i2c(), 0x01, 0x80);
-	fp_sendcmd(fp_get_i2c(), 0x26, 0x80); // Mhhh .. muesste das nicht an 0x01 bei Nokia? Geht aber beides nicht mit einer Sagem FB
+		dbox2_fp_queue_alloc(3, dbox2_fp_new_rc_queue_handler);
+
+	/* Enable break codes */
+	fp_sendcmd(fp_get_i2c(), 0x26, 0x80);
 
 	return 0;
-
 }
 
 void __exit dbox2_fp_rc_exit(void)
 {
-
 	if (old_rc)
 		dbox2_fp_queue_free(0);
 
 	if (new_rc)
 		dbox2_fp_queue_free(3);
 
-	// Disable break codes	
-	//fp_sendcmd(fp_get_i2c(), 0x01, 0x00);
-	fp_sendcmd(fp_get_i2c(), 0x26, 0x00); // Mhhh .. muesste das nicht an 0x01 bei Nokia? Geht aber beides nicht mit einer Sagem FB
-
+	/* Disable break codes */
+	fp_sendcmd(fp_get_i2c(), 0x26, 0x00);
 }
 
 #ifdef MODULE
